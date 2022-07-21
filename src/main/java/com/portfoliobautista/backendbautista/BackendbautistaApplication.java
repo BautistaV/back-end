074f492876dd73370c089fc97cecf6633da3b237
BautistaV <98930866+BautistaV@users.noreply.github.com>
2022-07-21 18:02:06 -0300

    Initial commit

---
+++ src/main/java/com/portfoliobautista/backendbautista/BackendbautistaApplication.java
@@ -0,0 +1,13 @@
+package com.portfoliobautista.backendbautista;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendbautistaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendbautistaApplication.class, args);
+	}
+
+}
